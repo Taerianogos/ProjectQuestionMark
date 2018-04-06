dacdbabd15249b9f007c757389c80a24f258ab52
Taerianogos <cucust987@gmail.com>
2018-04-06 11:59:53 +0300

    Attempt at a hex map
    Needs better hex model

---
+++ Project QuestionMark/Assets/MapGenerator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour {
+
+    public GameObject hexPrefab;
+    public int mapLength, mapWidth;
+    float zOffset = 0.882f;
+    float xOffset = 0.764f;
+
+	void Start () {
+        for (int x = 0; x < mapWidth; x++)
+            for (int y = 0; y < mapLength; y++)
+            {
+                float xPos = x * xOffset;
+                if (y % 2 == 1)
+                    xPos += xOffset / 2f;
+                GameObject newHex = (GameObject)Instantiate(hexPrefab, new Vector3(xPos, 0, y * zOffset), Quaternion.identity);
+                newHex.name = "Hex_" + x + "_" + y;
+                newHex.transform.SetParent(this.transform);
+            }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
